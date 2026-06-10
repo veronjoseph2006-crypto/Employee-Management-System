b7c7c365c8eeb31235283f8da10c42d067297d3d
veronjoseph2006 <veronjoseph2006@gmail.com>
2026-06-10 21:38:54 +0530

    Created employee API endpoint

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,20 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("<h1>Employee Management System Backend Running</h1>");
 });
+app.get("/employees", (req, res) => {
+    res.json([
+        {
+            id: "EMP001",
+            name: "John Doe",
+            department: "IT"
+        },
+        {
+            id: "EMP002",
+            name: "Jane Smith",
+            department: "HR"
+        }
+    ]);
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
