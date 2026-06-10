d800ebad9a98832f600f2cdfabe8e0dcce3b4de4
veronjoseph2006 <veronjoseph2006@gmail.com>
2026-06-10 21:25:45 +0530

    Configured Express server response

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Employee Management System Server Running");
+    res.send("<h1>Employee Management System Backend Running</h1>");
 });
 
 app.listen(PORT, () => {
