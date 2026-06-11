d5fdd3d2add3c4208f6c5f424f3e4c4479b29848
veronjoseph2006 <veronjoseph2006@gmail.com>
2026-06-11 10:32:28 +0530

    Connected SQLite database and created employees table

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const db = require("../database/database");
 
 const app = express();
 
 const PORT = 3000;
+db.run(`
+    CREATE TABLE IF NOT EXISTS employees (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        employee_id TEXT,
+        name TEXT,
+        department TEXT,
+        position TEXT
+    )
+`);
 
 app.get("/", (req, res) => {
     res.send("<h1>Employee Management System Backend Running</h1>");
